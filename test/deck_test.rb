ce4a07abaea1ed02d309d533279784ac6d827c1d
Michael Gallup <Gallup93@icloud.com>
2020-03-21 15:20:49 -0600

    addedrank_of_card method to deck.rb, corresponding test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -18,6 +18,19 @@ class CardTest < Minitest::Test
   end
 
 
+  def test_rank_of_card_at
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    assert_equal 12, deck.rank_of_card_at(0)
+    assert_equal 14, deck.rank_of_card_at(2)
+  end
+
+
 
 
 end
