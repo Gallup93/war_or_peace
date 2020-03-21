171021d73063a40ec4592f5664cd4413a9185bbd
Michael Gallup <Gallup93@icloud.com>
2020-03-21 14:57:55 -0600

    added deck.rb with initialize method, deck_test.rb with initialize test method

---
+++ test/deck_test.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+
+class CardTest < Minitest::Test
+
+  def test_if_exists
+
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    assert_instance_of Deck, deck
+  end
+
+
+
+
+end
