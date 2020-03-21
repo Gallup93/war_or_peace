8e0266377b5e352d411fe2c92605e24086cf4306
Michael Gallup <Gallup93@icloud.com>
2020-03-21 14:54:41 -0600

    added card.rb with initialize method and test method in card_test.rb

--- test/card_test.rb
+++ test/card_test.rb
@@ -3,9 +3,11 @@ require 'minitest/pride'
 require './lib/card'
 
 class CardTest < Minitest::Test
-  def setup
-    card = Card.new(:diamond, 'Queen', 12)
-  end
+  # def setup
+  #   p "test-SETUP"
+  #   card = Card.new(:diamond, 'Queen', 12)
+  #   require "pry"; binding.pry
+  # end
 
   def test_it_exists
     card = Card.new(:diamond, 'Queen', 12)
@@ -15,9 +17,10 @@ class CardTest < Minitest::Test
 
   def test_it_has_readable_attributes
     card = Card.new(:diamond, 'Queen', 12)
-    
+
     assert_equal :diamond, card.suit
     assert_equal 'Queen', card.value
     assert_equal 12, card.rank
   end
+
 end
