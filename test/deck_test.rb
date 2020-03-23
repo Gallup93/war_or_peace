d7ad56cdffe7259db903b6bcf3e7405d3fb2a718
Michael Gallup <Gallup93@icloud.com>
2020-03-22 22:41:28 -0600

    added comment code

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,8 +5,8 @@ require './lib/deck'
 
 class CardTest < Minitest::Test
 
+  #tests instance of Deck
   def test_if_exists
-
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
@@ -17,6 +17,7 @@ class CardTest < Minitest::Test
     assert_instance_of Deck, deck
   end
 
+  #tests rank_of_card_at method
   def test_rank_of_card_at
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
@@ -29,6 +30,7 @@ class CardTest < Minitest::Test
     assert_equal 14, deck.rank_of_card_at(2)
   end
 
+  #tests high_ranking_cards method
   def test_high_ranking_cards
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
@@ -40,6 +42,7 @@ class CardTest < Minitest::Test
     assert_equal [card1, card3], deck.high_ranking_cards
   end
 
+  #test percent_high_ranking method
   def test_percent_high_ranking
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
@@ -51,6 +54,7 @@ class CardTest < Minitest::Test
     assert_equal 66.67, deck.percent_high_ranking
   end
 
+  #test remove_card method
   def test_remove_card
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
@@ -59,10 +63,10 @@ class CardTest < Minitest::Test
 
     deck = Deck.new(cards)
 
-
     assert_equal card1, deck.remove_card
   end
 
+  #test add_card method
   def test_add_card
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
